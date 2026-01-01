c9e2db358bbf81299614f77386f080adf2463f80
Daniel Santos <daniel.santos@dss.tec.br>
2026-01-01 18:52:54 -0300

    Inclusão das rotas do modelo User

--- dundie/db.py
+++ dundie/db.py
@@ -1,9 +1,16 @@
 """Database connection"""
 from sqlmodel import create_engine
 from .config import settings
+from sqlmodel import Session
 
 engine = create_engine(
     settings.db.uri,  # pyright: ignore
     echo=settings.db.echo,  # pyright: ignore
     connect_args=settings.db.connect_args,  # pyright: ignore
 )
+
+def get_db_session():
+    """Get a new session"""
+    with Session(engine) as session:
+        yield session
+
